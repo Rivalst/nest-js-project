20ba4e3822691976673437a0daf95b242dbf9430
Rivalst <mr.ryabchits@gmail.com>
2025-01-29 11:23:02 +0200

    fix: fix transform dto

--- src/blog/client_blog/dto/create-blog.dto.ts
+++ src/blog/client_blog/dto/create-blog.dto.ts
@@ -1,5 +1,6 @@
 import { IsArray, IsInt, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class CreateBlogDto {
   @IsString()
@@ -16,6 +17,7 @@ export class CreateBlogDto {
 
   @IsArray()
   @IsInt({ each: true })
+  @Transform(({ value }) => value.map(Number))
   @ApiProperty({ type: [Number], description: 'category ids' })
   categoryIds?: number[];
 }
